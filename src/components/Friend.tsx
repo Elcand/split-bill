8060ec7e594cb70e1f5d7998d04a854485a78d2f
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-11 16:05:27 +0700

    make desain n show friend list

---
+++ src/components/Friend.tsx
@@ -0,0 +1,20 @@
+export default function Friend({ friend }: any) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          Kamu Berhutang Rp{Math.abs(friend.balance)} ke {friend.name}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} Berhutang Rp{Math.abs(friend.balance)} ke kamu
+        </p>
+      )}
+      {friend.balance === 0 && <p>Kamu dan {friend.name} Tidak Berhutang</p>}
+      <button className="button">Pilih</button>
+    </li>
+  );
+}
