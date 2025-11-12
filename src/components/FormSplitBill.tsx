a7336bab64e38c2cb7c72bf6efa624a606e7ed78
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-12 13:33:42 +0700

    make logic form split bill

--- src/components/FormSplitBill.tsx
+++ src/components/FormSplitBill.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 export default function FormSplitBill({ selectedFriend }: any) {
+  const [amount, setAmount] = useState("");
+  const [myBill, setMyBill] = useState("");
+  const friendBill = amount ? amount - myBill : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form action="" className="form-split-bill">
       <h2> Patungan Bareng {selectedFriend.name}</h2>
 
       <label htmlFor="">💵Total Tagihan</label>
-      <input type="text" name="" id="" />
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+
       <label htmlFor="">🙋‍♂️Tagihan Kamu</label>
-      <input type="text" name="" id="" />
+      <input
+        type="text"
+        value={myBill}
+        onChange={(e) => setMyBill(e.target.value)}
+      />
+
       <label htmlFor="">🙋Tagihan {selectedFriend.name}</label>
-      <input type="text" name="" id="" disabled />
+      <input type="text" value={friendBill} disabled />
 
       <label htmlFor="">🤑 Ditalangin Sama</label>
-      <select name="" id="">
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">Kamu</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
